2c24f568e11b21264e0c9cf3cb3ed41d7f1b8bd9
Matthias Jäger <write@matthias-jaeger.net>
2018-02-18 14:50:11 +0100

    Updated variables

--- scripts/render.js
+++ scripts/render.js
@@ -1,45 +1,45 @@
 /*
-*  renders the resulting grid to the canvas
-*  
+*  Renders the results of a call to calc() as a grid to the canvas.
+*  Each cell is given an outline and a label, containing it's index.
+*
 *  @params  (elements) Number of elements given as positve integer
-*           (gridwidth) windowWidth, calculated by p5, in pixels
-*           (gridheight) windowHeight, calculated by p5, in pixels
+*           (gridw) windowWidth, calculated by p5, in pixels
+*           (gridh) windowHeight, calculated by p5, in pixels
 *
 *  @author   Matthias Jäger
 */
 
-function renderSquarePack(elements, gridwidth, gridheight) {
-   // calling calc(), see libraries/square_pack.js
-   // stores the returned results in grid
-   let grid = calc(elements, gridwidth, gridheight);
+function renderSquarePack(elements, gridw, gridh) {
+   // call to calc()
+   let grid = calc(elements, gridw, gridh);
+   // returned results stored in grid
    let scl = grid.scl;
    let cols = grid.col;
    let rows = grid.row;
 
-   // only display grid cells
-   let counter = 0;
+   // lable grid cells
+   let index = 1;
 
-   // draws the whole grid with cols and rows
+   // draw the results as a grid
    for (let row = 0; row < rows; row++) {
       for (let col = 0; col < cols; col++) {
-         // calculate x, y coordinates for all cells
+         // calculate coordinates
          let x = col * scl;
          let y = row * scl;
-         // but there are possibly more cells than elements
-         if (counter < elements) {
+         // only draw actual elements
+         if (index <= elements) {
+            // outline square
             fill(255);
             rect(x, y, scl, scl);
+            // label square
+            fill(0);
+            textSize(map(scl, height, 1, 160, 5));
+            text(index, x + scl/2, y + scl/2);
          } else {
+            // No more elements to render
             break;
          }
-         // only show visible text
-         if (scl > 20) {
-            fill(0);
-            // responsive text size
-            textSize(map(scl, height, 1, 160, 5));
-            text(counter+1, x + scl/2, y + scl/2);
-         }
-         counter++;
+         index++;
       }
    }
 }
